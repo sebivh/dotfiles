b7a77d4cac3b09442eae410933cff3fa87bbc569
sebivh <sebivhelm@gmail.com>
2025-08-17 18:46:14 +0200

    Configure VimTex with wright build dirs

--- nvim/lua/core/plugins/vimtex.lua
+++ nvim/lua/core/plugins/vimtex.lua
@@ -5,5 +5,19 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "skim"
+	vim.g.vimtex_compiler_latexmk = {
+		aux_dir = './build/aux',       -- auxiliary files directory
+		out_dir = './build',       -- output directory (PDF)
+		callback = 1,
+		continuous = 1,
+		executable = 'latexmk',
+		hooks = {},
+		options = {
+			'-verbose',
+			'-file-line-error',
+			'-synctex=1',
+			'-interaction=nonstopmode',
+		},
+}
   end
 }
